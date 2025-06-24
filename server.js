7d062447b55fd3b9662f37194f049c5304171d9b
Dilek Gül <110824862+dilekgl@users.noreply.github.com>
2025-06-24 16:55:26 +0300

    .

--- server.js
+++ server.js
@@ -40,4 +40,4 @@ app.post("/send", async (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Sunucu ${PORT} portunda çalışıyor`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
